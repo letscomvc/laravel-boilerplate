cbb7830ad7007645dd2a032e66825b203f238f93
Danilo Pinotti <danilopinotti@letsgrow.com.br>
2018-01-23 15:58:11 -0200

    Search insensitive

--- project/app/Models/User.php
+++ project/app/Models/User.php
@@ -20,7 +20,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $searchBy = [
-        'name', 'email'
+        'name', 'email',
     ];
 
     /**
--- project/app/Scopes/Search.php
+++ project/app/Scopes/Search.php
@@ -4,10 +4,31 @@ namespace App\Scopes;
 
 trait Search
 {
+    private function addConditionSensitive($query, $column, $search)
+    {
+        $query->orWhere($column, 'LIKE', "%$search%");
+    }
+
+    private function addConditionInsensitive($query, $column, $search)
+    {
+        $query->orWhereRaw("LOWER({$column}) LIKE LOWER(?)", "%$search%");
+    }
+
     private function addConditions($query, $search, $searchBy)
     {
-        foreach ($searchBy as $field) {
-            $query->orWhere($field, 'LIKE', "%$search%");
+        foreach ($searchBy as $key => $field) {
+            $hasNoCase = is_numeric($key);
+            if ($hasNoCase) {
+                $this->addConditionInsensitive($query, $field, $search);
+            } else {
+                if ($field == 'sensitive') {
+                    $this->addConditionSensitive($query, $key, $search);
+                } elseif ($field == 'insensitive') {
+                    $this->addConditionInsensitive($query, $key, $search);
+                } else {
+                    throw new InvalidArgumentException("Invalid search case");
+                }
+            }
         }
 
         return $query;
@@ -18,11 +39,11 @@ trait Search
         if (isset($this->searchBy)) {
             if (is_array($this->searchBy)) {
                 $searchBy = $this->searchBy;
-                $query->where(function($query) use ($search, $searchBy) {
+                $query->where(function ($query) use ($search, $searchBy) {
                     $this->addConditions($query, $search, $searchBy);
                 });
             } else {
-                $query->where($this->searchBy, 'LIKE', "%$search%");
+                $this->addConditionInsensitive($query, $this->searchBy, $search);
             }
         }
     }
