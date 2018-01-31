87b96f86c68ba1ef5c4aee16366759ae23e1f71e
Danilo Pinotti <danilopinotti@letsgrow.com.br>
2018-01-31 18:12:20 -0200

    Comando: php artisan test

--- project/routes/console.php
+++ project/routes/console.php
@@ -16,3 +16,8 @@ use Illuminate\Foundation\Inspiring;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
+
+Artisan::command('test', function () {
+    $command = './vendor/bin/phpunit';
+    pcntl_exec($command);
+})->describe('Run PHPUnit');
