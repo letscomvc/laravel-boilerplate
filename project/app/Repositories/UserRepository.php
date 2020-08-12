21f20c977904b3d461d9b02bfc7b85757d40146d
Luiz Eduardo Chicouski da Cruz <luizcruz@letsgrow.com.br>
2020-08-12 11:29:50 -0300

    Override methods to apply password hash

--- project/app/Repositories/UserRepository.php
+++ project/app/Repositories/UserRepository.php
@@ -9,4 +9,38 @@ class UserRepository extends Repository
     {
         return User::class;
     }
+
+    public function create($data)
+    {
+        $data = $this->hashPassword($data);
+
+        return parent::create($data);
+    }
+
+    public function firstOrCreate($data)
+    {
+        $data = $this->hashPassword($data);
+
+        return parent::firstOrCreate($data);
+    }
+
+    public function update($id, $data)
+    {
+        $data = $this->hashPassword($data);
+
+        return parent::update($id, $data);
+    }
+
+    private function hashPassword($data)
+    {
+        $data['password'] = $data['password'] ?? null;
+        if ($data['password']) {
+            unset($data['password']);
+            unset($data['password_confirmation']);
+        } else {
+            $data['password'] = \Hash::Make($data['password']);
+        }
+
+        return $data;
+    }
 }
