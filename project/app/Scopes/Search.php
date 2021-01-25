c37c0a45fc9b36602e2123280f60f0613404741b
Danilo Pinotti <danilopinotti@hotmail.com>
2021-01-25 09:35:42 -0300

    Atualizar busca

--- project/app/Scopes/Search.php
+++ project/app/Scopes/Search.php
@@ -2,46 +2,65 @@
 
 namespace App\Scopes;
 
+use Illuminate\Database\PostgresConnection;
+
 trait Search
 {
-    private function addConditions($query, $search, $searchBy)
-    {
-        $searchBy = array_filter($searchBy);
-        foreach ($searchBy as $key => $field) {
-            $query->orWhereRaw("CAST({$field} as VARCHAR) ILIKE ?", "%$search%");
+    public function scopeSearch(
+        $query,
+        string $term,
+        ?array $searchBy = null,
+        ?array $searchByRelationship = null
+    ) {
+
+        if ($searchBy !== null) {
+            $this->searchBy = $searchBy;
         }
+
+        if ($searchByRelationship !== null) {
+            $this->searchByRelationship = $searchByRelationship;
+        }
+
+        $query->where(function ($query) use ($term) {
+            if (isset($this->searchBy) && is_array($this->searchBy)) {
+                $this->addConditions($query, $term, $this->searchBy);
+            }
+
+            if (isset($this->searchByRelationship) && is_array($this->searchByRelationship)) {
+                $this->addConditionsForRelationships($query, $term, $this->searchByRelationship);
+            }
+        });
 
         return $query;
     }
 
-    private function addConditionsForRelationships($query, $search, $relationships)
+    private function addConditions($query, $term, $searchBy)
+    {
+        $terms = str_getcsv($term, ' ', '"');
+        collect($terms)
+            ->filter()
+            ->each(function ($term) use ($searchBy, $query) {
+                foreach (array_filter($searchBy) as $key => $field) {
+                    (!$query->getConnection() instanceof PostgresConnection)
+                        ? $query->orWhereRaw("{$field} LIKE ?", "$term%")
+                        : $query->orWhereRaw("CAST({$field} as VARCHAR) ILIKE ?", "$term%");
+                }
+            });
+
+        return $query;
+    }
+
+    private function addConditionsForRelationships($query, $term, $relationships)
     {
         $relationships = array_filter($relationships);
         foreach ($relationships as $relation => $fields) {
-            $query->orWhereHas($relation, function ($query) use ($fields, $search) {
-                $query->where(function ($query) use ($fields, $search) {
-                    $this->addConditions($query, $search, $fields);
+            $query->orWhereHas($relation, function ($query) use ($fields, $term) {
+                $query->where(function ($query) use ($fields, $term) {
+                    $this->addConditions($query, $term, $fields);
                 });
             });
         }
 
         return $query;
-    }
-
-    public function scopeSearch($query, $search)
-    {
-        if (method_exists(get_called_class(), 'setupSearch')) {
-            $this->setupSearch();
-        }
-
-        $query->where(function ($query) use ($search) {
-            if (isset($this->searchBy) && is_array($this->searchBy)) {
-                $this->addConditions($query, $search, $this->searchBy);
-            }
-
-            if (isset($this->searchByRelationship) && is_array($this->searchByRelationship)) {
-                $this->addConditionsForRelationships($query, $search, $this->searchByRelationship);
-            }
-        });
     }
 }
